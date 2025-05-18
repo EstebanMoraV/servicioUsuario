c70375f19e8689a85e709a982e13ebe3f3ae2aec
juanjosesoto29 <sotojuaco@gmail.com>
2025-05-18 17:03:03 -0400

    Se creo clase UsuarioService, y se implementa lógica CRUD en UsuarioService.

--- src/main/java/com/usuario/servicioUsuario/service/UsuarioService.java
+++ src/main/java/com/usuario/servicioUsuario/service/UsuarioService.java
@@ -1,5 +1,30 @@
 package com.usuario.servicioUsuario.service;
 
-public class UsuarioService {
+import com.usuario.servicioUsuario.model.Usuarios;
+import com.usuario.servicioUsuario.repository.UsuarioRepository
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UsuarioService {
+    @Autowired
+    private InterfaceUsuarios usuarios;
+    
+    public List<Usuarios> findAll() {
+        return UsuarioRepository.findAll();
+    }
+    public Usuarios findById(Long id) {
+        return UsuarioRepository.findById(id).get();
+    }
+    public void save(Usuarios usuarios) {
+        return UsuarioRepository.save(usuarios);
+    }
+    public void delete(Long id) {
+        return UsuarioRepository.deleteById(id);
+    }
 }
