cc5a0c6f372ee28f053da9eea6c1d6ec0a007101
Esteban <esteban.morauc@gmail.com>
2025-05-18 15:05:40 -0400

    Creación del microservicio Usuario

---
+++ src/main/java/com/usuario/servicioUsuario/ServicioUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.usuario.servicioUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usuario/servicioUsuario/ServicioUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuario.servicioUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
