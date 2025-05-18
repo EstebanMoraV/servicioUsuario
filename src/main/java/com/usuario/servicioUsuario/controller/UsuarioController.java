83fc4cbb9fefd7159be9603533e21b9f61957238
Esteban <esteban.morauc@gmail.com>
2025-05-18 17:34:16 -0400

    Controlador creado

--- src/main/java/com/usuario/servicioUsuario/controller/UsuarioController.java
+++ src/main/java/com/usuario/servicioUsuario/controller/UsuarioController.java
@@ -1,5 +1,84 @@
 package com.usuario.servicioUsuario.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.usuario.servicioUsuario.model.Usuarios;
+import com.usuario.servicioUsuario.service.UsuarioService;
+
+@RestController
+@RequestMapping("/api/v1/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+    @GetMapping
+    public ResponseEntity<List<Usuarios>> getAllUsuarios() {
+        List<Usuarios> usuarios = usuarioService.findAll();
+        if (usuarios.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuarios> getUsuarioById(@PathVariable Long id) {
+        Usuarios usuario = usuarioService.findById(id);
+        if (usuario == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuarios> createUsuario(@RequestBody Usuarios usuario) {
+        try {
+            Usuarios createdUsuario = usuarioService.save(usuario);
+            return new ResponseEntity<>(createdUsuario, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuarios> updateUsuario(@PathVariable Long id, @RequestBody Usuarios usuario) {
+        Usuarios user = usuarioService.findById(id);
+        try {
+            if (user == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            user.setNombreUsuario(usuario.getNombreUsuario());
+            user.setContrasena(usuario.getContrasena());
+            user.setCorreo(usuario.getCorreo());
+            user.setFechaNacimiento(usuario.getFechaNacimiento());
+            //n Falta agregar fecha de creacion
+            Usuarios actualizUsuarios = usuarioService.save(user);
+            return new ResponseEntity<>(actualizUsuarios, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteUsuario(@PathVariable Long id) {
+        try {
+            usuarioService.delete(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
